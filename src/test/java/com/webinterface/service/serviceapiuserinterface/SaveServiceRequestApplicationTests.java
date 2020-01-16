7de0b3af742447035f968ebadc13f76fb7d08232
kishorepatha <kishorepatha@kishores-mbp.cgocable.net>
2020-01-15 23:27:13 -0500

    .

---
+++ src/test/java/com/webinterface/service/serviceapiuserinterface/SaveServiceRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webinterface.service.serviceapiuserinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveServiceRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
